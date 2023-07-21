704923cccb694cda51b6f8dbdb737e5717749e73
MatiasIcardo <matuicardo@gmail.com>
2023-07-21 19:11:42 -0300

    ultimaEntregaIcardo

--- src/componentes/Checkout/Checkout.jsx
+++ src/componentes/Checkout/Checkout.jsx
@@ -55,47 +55,48 @@ const Checkout = () => {
             <h2>Checkout</h2>
             <div className="container">
                 <div className="row">
-            <form onSubmit={manejadorFormulario}>
-                {carrito.map(producto => (
-                    <div>
-                        <p>
-                            {producto.item.nombre} x {producto.item.cantidad}
-                        </p>
-                        <p>Precio ${producto.item.precio}</p>
-                        total
-                        <hr />
-                    </div>
-                ))}
-                <div>
-                    <label htmlFor="nombre" className="form-label"> Nombre </label>
-                    <input type="text" className="form-control" value={nombre} onChange={(e) => setNombre(e.target.value)} />
-                </div>
+                    <form onSubmit={manejadorFormulario}>
+                        {carrito.map(producto => (
+                            <div>
+                                <p>
+                                    {producto.item.nombre} x {producto.cantidad}
+                                </p>
+                                <p>Precio ${producto.item.precio}</p>
+                                <strong>Total a pagar: $ {total}</strong>
+                                <hr />
 
-                <div>
-                    <label htmlFor="" className="form-label"> Apellido </label>
-                    <input type="text" className="form-control" value={apellido} onChange={(e) => setApellido(e.target.value)} />
-                </div>
+                            </div>
+                        ))}
+                        <div>
+                            <label htmlFor="nombre" className="form-label"> Nombre </label>
+                            <input type="text" className="form-control" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+                        </div>
 
-                <div>
-                    <label htmlFor="" className="form-label"> Telefono </label>
-                    <input type="text" className="form-control" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
-                </div>
+                        <div>
+                            <label htmlFor="" className="form-label"> Apellido </label>
+                            <input type="text" className="form-control" value={apellido} onChange={(e) => setApellido(e.target.value)} />
+                        </div>
 
-                <div>
-                    <label htmlFor="" className="form-label"> Email </label>
-                    <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} />
-                </div>
+                        <div>
+                            <label htmlFor="" className="form-label"> Telefono </label>
+                            <input type="text" className="form-control" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
+                        </div>
 
-                <div>
-                    <label htmlFor="" className="form-label"> Email Confirmación </label>
-                    <input type="email" className="form-control" value={emailConfirmacion} onChange={(e) => setEmailConfirmacion(e.target.value)} />
+                        <div>
+                            <label htmlFor="" className="form-label"> Email </label>
+                            <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </div>
+
+                        <div>
+                            <label htmlFor="" className="form-label"> Email Confirmación </label>
+                            <input type="email" className="form-control" value={emailConfirmacion} onChange={(e) => setEmailConfirmacion(e.target.value)} />
+                        </div>
+                        {
+                            error && <p> {error}</p>
+                        }
+                        <button type="submit">Finalizar pedido</button>
+                    </form>
                 </div>
-                {
-                    error && <p> {error}</p>
-                }
-                <button type="submit">Finalizar pedido</button>
-            </form>
-            </div>
             </div>
 
             {
